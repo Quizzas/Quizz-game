4e06ed4147316e030292a2272118e12c3f04c4ea
unknown <marciusss1@gmail.com>
2022-12-15 17:51:58 +0200

    selectinau HTML elements

---
+++ src/modules/selectElements.js
@@ -0,0 +1,27 @@
+
+ const listItems = document.getElementsByClassName("top-container")
+ console.log(listItems)
+
+ const listItems1 = document.getElementsByClassName("question")
+ console.log(listItems1)
+
+ const listItems2 = document.getElementsByClassName("text")
+ console.log(listItems2)
+
+ const listItems3 = document.getElementsByClassName("bottom-container")
+ console.log(listItems3)
+
+ const listItems4 = document.getElementsByClassName("bar")
+ console.log(listItems4)
+
+ const listItems5 = document.getElementsByClassName("progress")
+ console.log(listItems5)
+
+ const listItems6 = document.getElementsByClassName("start")
+ console.log(listItems6)
+
+ const listItems7 = document.getElementsByClassName("restart")
+ console.log(listItems7)
+
+ const listItems8 = document.getElementsByClassName("fifty")
+ console.log(listItems8)
