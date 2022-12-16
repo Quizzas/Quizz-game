30313a06ebba5201d0d29beff1a67a7e96004b61
unknown <marciusss1@gmail.com>
2022-12-16 14:09:08 +0200

    checke

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-import { questions } from "./data.js";
+import { questions } from "./data.js"; 
+//import {} from "./answerChecker.js";
+import {p} from "./qNumberDinamic.js";
 
 console.log(questions);
+console.log(p);
+document.querySelector(".progress").innerHTML = p;
