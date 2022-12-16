e33e152d22acbcd9004c7b07574a7a92d4b36d8e
MarcinkEve <marcinke@gmail.com>
2022-12-16 15:00:34 +0200

    main restored

--- src/answerChecker.js
+++
@@ -1,7 +0,0 @@
-if (answer == true){
-    console.log("green")
-    document.sk1.style.backgroundColor = "green";
-}
-else{
-    document.sk1.style.backgroundColor = "red";
-}
--- src/qNumberDinamic.js
+++
@@ -1,5 +0,0 @@
-export let p=""
-for(let i=1; i<=10;i++){
-    p+="<span class='sk"+i+"'>"+i+"</span>";
-}
-document.getElementsByClassName("progress").innerHTML = p;
