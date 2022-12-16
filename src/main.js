7ff4a799bc3bca090da7835bfe494ff6518fa778
Geraseo <tomt78872@gmail.com>
2022-12-16 12:51:28 +0200

    restart button functionality

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import { questions } from "./data.js";
 
-import { dataCopy } from "./modules/q&aDisplay.js"
+import { dataCopy } from "./modules/q&aDisplay.js";
 
-import { displayProgressBar } from "./modules/progressBarDisplay.js"
+import { displayProgressBar } from "./modules/progressBarDisplay.js";
 
-//layout stuff
+//*****************layout*******************
+//start
 const quizzDisplay = document.getElementById("quizz");
 const startContainer = document.getElementById("start-container");
 const startBtn = document.getElementById("startBtn");
@@ -13,7 +14,21 @@ startBtn.addEventListener("click", function () {
   startContainer.classList.add("d-none");
   quizzDisplay.classList.remove("d-none");
 });
+//end
+//************************************
 
-import { submit } from "./modules/q&aDisplay.js"
-submit()
-displayProgressBar()
+import { submit } from "./modules/q&aDisplay.js";
+submit();
+displayProgressBar();
+
+//*****************restart*******************
+//start
+const restartBtn = document.getElementById("restartBtn");
+
+restartBtn.addEventListener("click", function () {
+  startContainer.classList.remove("d-none");
+  startContainer.classList.add("d-block");
+  location.reload();
+});
+//end
+//************************************
